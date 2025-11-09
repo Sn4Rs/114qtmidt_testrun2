31703c1e49586672be5c2b0075c28c5545f87ff2
Sn4Rs <wisbi1997@gmail.com>
2025-11-09 14:11:08 +0800

    edit show window

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <QWidget>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
+    MainWindow w,w2;
+    w.move(100,100);
+    w2.move(500,100);
     w.show();
+    w2.show();
     return a.exec();
 }
